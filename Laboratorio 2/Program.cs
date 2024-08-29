113d7df036f886cabb95039e0b4010ef1e397b12
Gabriel Garcia <gabgac2000@outlook.com>
2024-08-29 00:10:18 -0500

    se creo otro documento con una segunda clase

---
+++ Laboratorio 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http.Json;
+
+namespace Laboratorio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            client client = new client();
+            //ejemplo utilizando las variables de instancia  de clase.
+            client.FirstName = "Gabriel";
+            client.LastName = "Garcia";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+        }
+
+        public class client
+        {
+            //Declarando variables de instancia en Clase. 
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public ushort Age { get; set; }
+
+            public string GetFullName()
+            {
+                //Utilizando variable de instamcia dentro de metodos de clase. 
+                return FirstName + " " + LastName;
+            }
+        }
+
+    }
+}
