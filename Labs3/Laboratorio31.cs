9609c16778ebd65efe015d760a882163190e2e6a
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-02 18:57:53 -0500

    prueba de taller

---
+++ Labs3/Laboratorio31.cs
@@ -0,0 +1,18 @@
+﻿internal class Laboratorio31
+{
+    private static void Main (string[] args)
+    {
+        int a, b, CalculosMatematicos;
+
+        Console.Write("Introduce el Primeron numero: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo Numero: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos = (a+b)*(a-b);
+
+        Console.WriteLine("El resultado del calculo es:" + CalculosMatematicos );
+
+    }
+}
