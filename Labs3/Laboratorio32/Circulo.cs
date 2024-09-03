f9715c553f2ee194325df060af9e6d69871af465
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-02 19:57:50 -0500

    se subio el laboratorio 32

---
+++ Labs3/Laboratorio32/Circulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculadoraArea
+{
+    public class Circulo
+    {
+        public double CalculoArea(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+    }
+}
--- Labs3/Laboratorio32/Program.cs
+++ Labs3/Laboratorio32/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculadoraArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circulo circulo = new Circulo();
+
+            Console.Write("Ingrese el radio del círculo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            double area = circulo.CalculoArea(radio);
+
+            Console.WriteLine($"El área del círculo con radio {radio} es: {area}");
+
+        }
+    }
+}
