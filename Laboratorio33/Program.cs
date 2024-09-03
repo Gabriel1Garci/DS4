0f2bfa4d51ebba58a48d95d9025860b5e235abef
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-02 20:05:25 -0500

    todos los labs

---
+++ Laboratorio33/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Introduce el largo del rectángulo: ");
+        double largo = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduce el ancho del rectángulo: ");
+        double ancho = Convert.ToDouble(Console.ReadLine());
+
+        double perimetro = 2 * (largo + ancho);
+
+        Console.WriteLine($"El perímetro del rectángulo es: {perimetro}");
+    }
+}
