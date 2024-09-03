0ef97836da03831a2d6d6b7a3ec2326661120c6d
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-02 19:46:37 -0500

    cambios en las carpetas

---
+++ Labs3/Laboratorio31/CalculosMatematicos.cs
@@ -0,0 +1,8 @@
+﻿internal class CalculosMatematicos
+{
+    public int Calcular(int a, int b)
+    {
+        return (a + b) * (a - b);
+    }
+
+}
---
+++ Labs3/Laboratorio31/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        int a, b, resultado; 
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos calculos = new CalculosMatematicos();
+
+        resultado = calculos.Calcular(a, b);
+
+        Console.WriteLine("El resultado de la operacion es: " + resultado);
+
+    }
+}
