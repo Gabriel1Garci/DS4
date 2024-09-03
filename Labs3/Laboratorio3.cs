9b25fe46f90b851e1b5f575816494c93e2ef9ea1
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-02 19:11:48 -0500

    arreglo de los problemas

---
+++ Labs3/Laboratorio3.cs
@@ -0,0 +1,18 @@
+﻿internal class Laboratorio3
+{
+    private static void Main (string[] args)
+    {
+        int primerNumero, segundoNumero, suma;
+
+        Console.Write("Introduce el Primeron numero: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo Numero: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        suma = primerNumero + segundoNumero;
+
+        Console.WriteLine("la suma de {0} y {1} es {2}", primerNumero, segundoNumero, suma); 
+
+    }
+}
