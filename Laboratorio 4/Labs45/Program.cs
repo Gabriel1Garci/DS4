095bf4f7ee2a52be2256bc4e91d762f103a845d5
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-09 20:00:52 -0500

    labs45 listo y funcionando

---
+++ Laboratorio 4/Labs45/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace labs45
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int edad1, edad2, edad3, edad4, edad5 = 31;
+
+            edad1 = edad2 = edad3 = edad4 = 32;
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", edad1, edad2, edad3, edad4, edad5);
+        }
+    }
+}
