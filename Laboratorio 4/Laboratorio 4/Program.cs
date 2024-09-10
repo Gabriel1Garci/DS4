cef0143e242da845ab3e5007c19c1437a50ab881
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-09 19:07:52 -0500

    primer problema resuelto 😁

---
+++ Laboratorio 4/Laboratorio 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EstructuraRepetoitivaWhile
+{
+
+    class Program
+    {
+        
+        static void Main (string[] args)
+        {
+
+            int n, x;
+            string linea;
+            Console.Write("Ingresar el valor de n:");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            x = 1;
+            while (x <= n)
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(" , ");
+                x = x + 1; 
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+}
