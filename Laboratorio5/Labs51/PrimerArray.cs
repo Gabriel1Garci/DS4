5f802f6b7ed86e2d386a18f0b1284e644ba19a46
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-11 20:01:45 -0500

    laboratorio 5 hecho

---
+++ Laboratorio5/Labs51/PrimerArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+internal class PrimerArray
+{
+    private int[] sueldos;
+
+    public void Cargar()
+    {
+        sueldos = new int[5];
+        for (int f = 0; f < sueldos.Length; f++)
+        {
+            Console.Write("Ingrese el sueldo del operario " + (f + 1) + ": ");
+            string linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);
+        }
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("Los 5 sueldos de los operarios son:");
+        for (int f = 0; f < sueldos.Length; f++) 
+        {
+            Console.Write("[" + sueldos[f] + "] ");
+        }
+        Console.ReadKey();
+    }
+
+    static void Main(string[] args)
+    {
+        PrimerArray pv = new PrimerArray();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}
