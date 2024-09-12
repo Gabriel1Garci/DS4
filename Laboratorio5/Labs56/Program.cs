ee663b607396a19a2ed37bd632dbce9a9603b6a8
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-12 00:10:24 -0500

    todo el labs completo

--- Laboratorio5/Labs56/Program.cs
+++ Laboratorio5/Labs56/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+        {
+            {"Francia", "París"},
+            {"España", "Madrid"},
+            {"Italia", "Roma"}
+        };
+
+        foreach (KeyValuePair<string, string> par in paisesYCapitales)
+        {
+            Console.WriteLine("La capital de " + par.Key + " es " + par.Value + ".");
+        }
+    }
+}
