71ef2a5c2003beea43cbe071da1670275c00694c
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-11 23:04:41 -0500

    problema 3 resuelto

---
+++ Laboratorio5/Labs53/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        String[] frutas = { "Manzana", "Platano", "Naranja" };
+
+        foreach (string fruta in frutas)
+        {
+
+            Console.WriteLine(fruta);
+        }
+    }
+}
