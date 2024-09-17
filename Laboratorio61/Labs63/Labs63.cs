24cfe8ed853a3928bbd0bf76b2c21ac8d6047af9
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-16 19:39:27 -0500

    laboratorio completo.

---
+++ Laboratorio61/Labs63/Labs63.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+class Labs63
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Algo salio mal, valide el indice del arreglo");
+        } 
+        finally
+        {
+            Console.WriteLine("continuacion de la aplicacion, luego del bloque try/CatchBlock");
+        }
+    }
+}
---
+++ Laboratorio61/Labs64/Labs64.cs
@@ -0,0 +1,19 @@
+﻿class Labs64
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso negado - no cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso Concedido.");
+
+        }
+    }
+    static void Main (string[] args)
+    {
+        checkAge(15);
+    }
+}
