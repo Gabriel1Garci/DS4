d756d318af46db8028e1afec2c7ae52b0f897eee
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-20 01:10:58 -0500

    primer problema resuelto

---
+++ labs8/Labs8/Colaborador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Labs8
+{
+    class Colaborador
+    {
+        public static void Main ()
+        {
+            Trabajador p = new Trabajador ("Jason", 22, "77588260", 100000);
+            Console.WriteLine("Nombre= " +p.Nombre);
+            Console.WriteLine("Edad= " + p.Edad);
+            Console.WriteLine("NIF= " + p.NIF);
+            Console.WriteLine("Sueldo= " + p.Sueldo);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ labs8/Labs8/Trabajador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs8
+{
+    class Trabajador : Persona
+    {
+        public int Sueldo;
+
+        public Trabajador(String nombre, int edad, string nif, int sueldo)
+            : base(nombre, edad, nif)
+        {
+            Sueldo = sueldo;
+        }
+    }
+}
