b30a7587a40f7f61ad1a3ff645c8d5bb1b3f1915
Gabriel Garcia <gabgac2000@outlook.com>
2024-09-25 22:40:35 -0500

    segundo problema recuelto

---
+++ Laboratorio9/Laboratorio92/Contar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Contar
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Los números pares del 1 al 100 son:");
+
+        for (int i = 1; i <= 100; i++) 
+        {
+            if (i % 2 == 0 || i % 3 == 0)
+                {
+                Console.Write(i+", "); 
+            }
+
+        }
+    }
+}
+
