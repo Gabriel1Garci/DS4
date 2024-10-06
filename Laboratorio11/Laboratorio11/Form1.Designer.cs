6acb606d8c5353c05e15b7cdbc024c585e8aca67
Gabriel Garcia <gabgac2000@outlook.com>
2024-10-06 13:06:42 -0500

    Laboratorio 11 listo.

---
+++ Laboratorio11/Laboratorio11/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Laboratorio11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCLickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCLickThis
+            // 
+            this.btnCLickThis.Location = new System.Drawing.Point(95, 57);
+            this.btnCLickThis.Name = "btnCLickThis";
+            this.btnCLickThis.Size = new System.Drawing.Size(151, 35);
+            this.btnCLickThis.TabIndex = 0;
+            this.btnCLickThis.Text = "CLICK THIS ";
+            this.btnCLickThis.UseVisualStyleBackColor = true;
+            this.btnCLickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(150, 108);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(0, 13);
+            this.lblHelloWorld.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 165);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnCLickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCLickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
---
+++ Parcial1/Parcial1/Parcial1.cs
@@ -0,0 +1,58 @@
+﻿class Parcial1
+{
+    static void Main() 
+    {
+        int numeroSolicitado;
+
+        do
+        {
+
+            Console.Write("Introduzca un Numero Impar para la Matriz: ");
+            numeroSolicitado = int.Parse(Console.ReadLine());
+
+            if (numeroSolicitado % 2 == 0)
+            {
+                Console.WriteLine("El numero debe de ser impar, Intentar denuevo.");
+            }
+
+        } while (numeroSolicitado % 2 == 0);
+
+        int[,] matriz = new int[numeroSolicitado, numeroSolicitado];
+        Random random = new Random();
+        int sumaColumna = 0, sumaFila = 0;
+        int indicemedio = numeroSolicitado / 2;
+
+        for (int i = 0; i < numeroSolicitado; i++)
+        {
+            for (int j = 0; j < numeroSolicitado; j++)
+            {
+                if (i == indicemedio || j == indicemedio )
+                {
+                    matriz[i, j] = random.Next(1, 101);
+                    if (i == indicemedio) sumaFila += matriz[i, j];
+                    if (j == indicemedio) sumaColumna += matriz[i, j];
+
+                }
+                else
+                {
+                    matriz[i, j] = 0;
+                }
+            }
+        }
+
+        Console.WriteLine("\nResultado de la Matriz en Forma de cruz");
+        for (int i = 0; i < numeroSolicitado; i++)
+        {
+            for (int j = 0; j < numeroSolicitado;j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nSuma de valores de la columna Solicitada " + sumaColumna);
+        Console.WriteLine(" Suma de los valores de la fila Solicitada " + sumaFila);
+
+    }
+}
+
