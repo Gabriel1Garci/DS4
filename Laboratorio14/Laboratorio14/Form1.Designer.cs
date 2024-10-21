f148e62d12895f028891da699d3995d45500ebb1
Gabriel Garcia <gabgac2000@outlook.com>
2024-10-21 14:28:22 -0500

    laboratorio14 corregido sin errores

---
+++ Laboratorio14/Laboratorio14/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Laboratorio14
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductos));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBuscaId = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.tsbCancelar = new System.Windows.Forms.PictureBox();
+            this.tsbGuardar = new System.Windows.Forms.PictureBox();
+            this.tsbEliminar = new System.Windows.Forms.PictureBox();
+            this.tsbNuevo = new System.Windows.Forms.PictureBox();
+            this.tsbBuscar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbCancelar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbGuardar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbEliminar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbNuevo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbBuscar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(343, 148);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Busca por ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Stock";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Precio";
+            // 
+            // txtBuscaId
+            // 
+            this.txtBuscaId.Location = new System.Drawing.Point(195, 12);
+            this.txtBuscaId.Name = "txtBuscaId";
+            this.txtBuscaId.Size = new System.Drawing.Size(100, 20);
+            this.txtBuscaId.TabIndex = 6;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 79);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(162, 79);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(256, 20);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(162, 151);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 9;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(12, 151);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 10;
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsbCancelar.Image")));
+            this.tsbCancelar.Location = new System.Drawing.Point(66, 5);
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(21, 29);
+            this.tsbCancelar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tsbCancelar.TabIndex = 11;
+            this.tsbCancelar.TabStop = false;
+            // 
+            // tsbGuardar
+            // 
+            this.tsbGuardar.Image = ((System.Drawing.Image)(resources.GetObject("tsbGuardar.Image")));
+            this.tsbGuardar.Location = new System.Drawing.Point(39, 5);
+            this.tsbGuardar.Name = "tsbGuardar";
+            this.tsbGuardar.Size = new System.Drawing.Size(21, 27);
+            this.tsbGuardar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tsbGuardar.TabIndex = 12;
+            this.tsbGuardar.TabStop = false;
+            // 
+            // tsbEliminar
+            // 
+            this.tsbEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEliminar.Image")));
+            this.tsbEliminar.Location = new System.Drawing.Point(93, 5);
+            this.tsbEliminar.Name = "tsbEliminar";
+            this.tsbEliminar.Size = new System.Drawing.Size(21, 29);
+            this.tsbEliminar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tsbEliminar.TabIndex = 13;
+            this.tsbEliminar.TabStop = false;
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNuevo.Image")));
+            this.tsbNuevo.Location = new System.Drawing.Point(12, 5);
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(21, 27);
+            this.tsbNuevo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tsbNuevo.TabIndex = 14;
+            this.tsbNuevo.TabStop = false;
+            // 
+            // tsbBuscar
+            // 
+            this.tsbBuscar.Image = ((System.Drawing.Image)(resources.GetObject("tsbBuscar.Image")));
+            this.tsbBuscar.Location = new System.Drawing.Point(301, 5);
+            this.tsbBuscar.Name = "tsbBuscar";
+            this.tsbBuscar.Size = new System.Drawing.Size(35, 29);
+            this.tsbBuscar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tsbBuscar.TabIndex = 15;
+            this.tsbBuscar.TabStop = false;
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 217);
+            this.Controls.Add(this.tsbBuscar);
+            this.Controls.Add(this.tsbNuevo);
+            this.Controls.Add(this.tsbEliminar);
+            this.Controls.Add(this.tsbGuardar);
+            this.Controls.Add(this.tsbCancelar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtBuscaId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Name = "frmProductos";
+            this.Text = "Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.tsbCancelar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbGuardar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbEliminar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbNuevo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tsbBuscar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBuscaId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.PictureBox tsbCancelar;
+        private System.Windows.Forms.PictureBox tsbGuardar;
+        private System.Windows.Forms.PictureBox tsbEliminar;
+        private System.Windows.Forms.PictureBox tsbNuevo;
+        private System.Windows.Forms.PictureBox tsbBuscar;
+    }
+}
+
--- Laboratorio14/Laboratorio14/Form1.cs
+++ Laboratorio14/Laboratorio14/Form1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -8,224 +7,226 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System;
 
 namespace Laboratorio14
 {
-    public partial class frmProductos : Form
-    {
-        private bool nuevo;
-        string connectionString = @"Server=localhost;Database=productos;Trusted_Connection=True;";
-        public frmProductos()
+        public partial class frmProductos : Form
         {
-            InitializeComponent();
-            nuevo = false;
-        }
-
-        private void tsbCancelar_Click(object sender, EventArgs e)
-        {
-            tsbNuevo.Enabled = true;
-            tsbGuardar.Enabled = false;
-            tsbCancelar.Enabled = false;
-            tsbEliminar.Enabled = false;
-            txtId.Enabled = false;
-            txtNombre.Enabled = false;
-            txtPrecio.Enabled = false;
-            txtStock.Enabled = false;
-            txtId.Text = "";
-            txtNombre.Text = "";
-            txtPrecio.Text = "";
-            txtStock.Text = "";
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void frmProductos_Load(object sender, EventArgs e)
-        {
-            tsbNuevo.Enabled = true;
-            tsbGuardar.Enabled = false;
-            tsbCancelar.Enabled = false;
-            tsbEliminar.Enabled = false;
-            txtId.Enabled = false;
-            txtNombre.Enabled = false;
-            txtPrecio.Enabled = false;
-            txtStock.Enabled = false;
-        }
-
-        private void tsbNuevo_Click(object sender, EventArgs e)
-        {
-            tsbNuevo.Enabled = false;
-            tsbGuardar.Enabled = true;
-            tsbCancelar.Enabled = true;
-            tsbEliminar.Enabled = false;
-            txtId.Enabled = false;
-            txtNombre.Enabled = true;
-            txtPrecio.Enabled = true;
-            txtStock.Enabled = true;
-            txtNombre.Focus();
-            nuevo = true;
-
-        }
-
-        private void tsbGuardar_Click(object sender, EventArgs e)
-        {
-            if (nuevo)
+            private bool nuevo;
+            string connectionString = @"Server=localhost;Database=productos;Trusted_Connection=True;";
+            public frmProductos()
             {
-                string sql = "INSERT INTO LAPTOPS (NOMBRE, PRECIO, STOCK) " +
-                             "VALUES ('" + txtNombre.Text + "', " + txtPrecio.Text + ", " + txtStock.Text + ")";
-                SqlConnection con = new SqlConnection(connectionString);
-                SqlCommand cmd = new SqlCommand(sql, con);
-                con.Open();
-                try
+                InitializeComponent();
+                nuevo = false;
+            }
+
+            private void tsbCancelar_Click(object sender, EventArgs e)
+            {
+                tsbNuevo.Enabled = true;
+                tsbGuardar.Enabled = false;
+                tsbCancelar.Enabled = false;
+                tsbEliminar.Enabled = false;
+                txtId.Enabled = false;
+                txtNombre.Enabled = false;
+                txtPrecio.Enabled = false;
+                txtStock.Enabled = false;
+                txtId.Text = "";
+                txtNombre.Text = "";
+                txtPrecio.Text = "";
+                txtStock.Text = "";
+
+            }
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void frmProductos_Load(object sender, EventArgs e)
+            {
+                tsbNuevo.Enabled = true;
+                tsbGuardar.Enabled = false;
+                tsbCancelar.Enabled = false;
+                tsbEliminar.Enabled = false;
+                txtId.Enabled = false;
+                txtNombre.Enabled = false;
+                txtPrecio.Enabled = false;
+                txtStock.Enabled = false;
+            }
+
+            private void tsbNuevo_Click(object sender, EventArgs e)
+            {
+                tsbNuevo.Enabled = false;
+                tsbGuardar.Enabled = true;
+                tsbCancelar.Enabled = true;
+                tsbEliminar.Enabled = false;
+                txtId.Enabled = false;
+                txtNombre.Enabled = true;
+                txtPrecio.Enabled = true;
+                txtStock.Enabled = true;
+                txtNombre.Focus();
+                nuevo = true;
+
+            }
+
+            private void tsbGuardar_Click(object sender, EventArgs e)
+            {
+                if (nuevo)
                 {
-                    int i = cmd.ExecuteNonQuery();
-                    if (i > 0)
+                    string sql = "INSERT INTO LAPTOPS (NOMBRE, PRECIO, STOCK) " +
+                                 "VALUES ('" + txtNombre.Text + "', " + txtPrecio.Text + ", " + txtStock.Text + ")";
+                    SqlConnection con = new SqlConnection(connectionString);
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    con.Open();
+                    try
                     {
-                        MessageBox.Show("Registro ingresado correctamente!");
+                        int i = cmd.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            MessageBox.Show("Registro ingresado correctamente!");
+                        }
                     }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.ToString());
-                }
-                finally
-                {
-                    con.Close();
-                }
-            }
-            else
-            {
-                string sql = "UPDATE LAPTOPS SET NOMBRE='" + txtNombre.Text + "', " +
-                             "PRECIO=" + txtPrecio.Text + ", " +
-                             "STOCK=" + txtStock.Text + " WHERE id=" + txtId.Text + "";
-                SqlConnection con = new SqlConnection(connectionString);
-                SqlCommand cmd = new SqlCommand(sql, con);
-                con.Open();
-                try
-                {
-                    int i = cmd.ExecuteNonQuery();
-                    if (i > 0)
+                    catch (Exception ex)
                     {
-                        MessageBox.Show("Registro actualizado correctamente!");
+                        MessageBox.Show("Error: " + ex.ToString());
                     }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.ToString());
-                }
-                finally
-                {
-                    con.Close();
-                }
-            }
-            tsbNuevo.Enabled = true;
-            tsbGuardar.Enabled = false;
-            tsbCancelar.Enabled = false;
-            tsbEliminar.Enabled = false;
-            txtId.Enabled = false;
-            txtNombre.Enabled = false;
-            txtPrecio.Enabled = false;
-            txtStock.Enabled = false;
-            txtId.Text = "";
-            txtNombre.Text = "";
-            txtPrecio.Text = "";
-            txtStock.Text = "";
-
-        }
-
-        private void tsbEliminar_Click(object sender, EventArgs e)
-        {
-            string sql = "DELETE FROM LAPTOPS WHERE id=" + this.txtId.Text + "";
-            SqlConnection con = new SqlConnection(connectionString);
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.CommandType = CommandType.Text;
-            con.Open();
-            try
-            {
-                int i = cmd.ExecuteNonQuery();
-                if (i > 0)
-                {
-                    MessageBox.Show("Registro eliminado correctamente!");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: " + ex.ToString());
-            }
-            finally
-            {
-                con.Close();
-            }
-            tsbNuevo.Enabled = true;
-            tsbGuardar.Enabled = false;
-            tsbCancelar.Enabled = false;
-            tsbEliminar.Enabled = false;
-            txtId.Enabled = false;
-            txtNombre.Enabled = false;
-            txtPrecio.Enabled = false;
-            txtStock.Enabled = false;
-            txtId.Text = "";
-            txtNombre.Text = "";
-            txtPrecio.Text = "";
-            txtStock.Text = "";
-
-        }
-
-        private void tsbBuscar_Click(object sender, EventArgs e)
-        {
-            string sql = "SELECT * FROM LAPTOPS WHERE ID=" + txtBuscaId.Text;
-            SqlConnection con = new SqlConnection(connectionString);
-            SqlCommand cmd = new SqlCommand(sql,con);
-            cmd.CommandType = CommandType.Text;
-            SqlDataReader reader;
-            con.Open();
-
-            try
-            {
-                reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    tsbNuevo.Enabled = false;
-                    tsbGuardar.Enabled = true;
-                    tsbCancelar.Enabled = true;
-                    tsbEliminar.Enabled = true;
-                    txtBuscaId.Enabled = false;
-                    tsbBuscar.Enabled = false;
-                    txtNombre.Enabled = true;
-                    txtPrecio.Enabled = true;
-                    txtStock.Enabled = true;
-                    txtNombre.Focus();
-                    txtId.Text = reader[0].ToString();
-                    txtNombre.Text = reader[1].ToString();
-                    txtPrecio.Text = reader[2].ToString();
-                    txtStock.Text = reader[3].ToString();
-                    nuevo = false;
+                    finally
+                    {
+                        con.Close();
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Ningún registro encontrado con el Id ingresado !");
+                    string sql = "UPDATE LAPTOPS SET NOMBRE='" + txtNombre.Text + "', " +
+                                 "PRECIO=" + txtPrecio.Text + ", " +
+                                 "STOCK=" + txtStock.Text + " WHERE id=" + txtId.Text + "";
+                    SqlConnection con = new SqlConnection(connectionString);
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    con.Open();
+                    try
+                    {
+                        int i = cmd.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            MessageBox.Show("Registro actualizado correctamente!");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.ToString());
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
                 }
+                tsbNuevo.Enabled = true;
+                tsbGuardar.Enabled = false;
+                tsbCancelar.Enabled = false;
+                tsbEliminar.Enabled = false;
+                txtId.Enabled = false;
+                txtNombre.Enabled = false;
+                txtPrecio.Enabled = false;
+                txtStock.Enabled = false;
+                txtId.Text = "";
+                txtNombre.Text = "";
+                txtPrecio.Text = "";
+                txtStock.Text = "";
+
             }
-            catch (Exception ex)
+
+            private void tsbEliminar_Click(object sender, EventArgs e)
             {
-                MessageBox.Show("Error: " + ex.ToString());
+                string sql = "DELETE FROM LAPTOPS WHERE id=" + this.txtId.Text + "";
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        MessageBox.Show("Registro eliminado correctamente!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+                tsbNuevo.Enabled = true;
+                tsbGuardar.Enabled = false;
+                tsbCancelar.Enabled = false;
+                tsbEliminar.Enabled = false;
+                txtId.Enabled = false;
+                txtNombre.Enabled = false;
+                txtPrecio.Enabled = false;
+                txtStock.Enabled = false;
+                txtId.Text = "";
+                txtNombre.Text = "";
+                txtPrecio.Text = "";
+                txtStock.Text = "";
+
             }
-            finally
+
+            private void tsbBuscar_Click(object sender, EventArgs e)
             {
-                con.Close();
+                string sql = "SELECT * FROM LAPTOPS WHERE ID=" + txtBuscaId.Text;
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader reader;
+                con.Open();
+
+                try
+                {
+                    reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        tsbNuevo.Enabled = false;
+                        tsbGuardar.Enabled = true;
+                        tsbCancelar.Enabled = true;
+                        tsbEliminar.Enabled = true;
+                        txtBuscaId.Enabled = false;
+                        tsbBuscar.Enabled = false;
+                        txtNombre.Enabled = true;
+                        txtPrecio.Enabled = true;
+                        txtStock.Enabled = true;
+                        txtNombre.Focus();
+                        txtId.Text = reader[0].ToString();
+                        txtNombre.Text = reader[1].ToString();
+                        txtPrecio.Text = reader[2].ToString();
+                        txtStock.Text = reader[3].ToString();
+                        nuevo = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ningún registro encontrado con el Id ingresado !");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+                txtBuscaId.Text = "";
+
             }
 
-            txtBuscaId.Text = "";
+            private void btnSalir_Click(object sender, EventArgs e)
+            {
+                this.Close();
 
+            }
         }
-
-        private void btnSalir_Click(object sender, EventArgs e)
-        {
-            this.Close();
-
-        }
-    }
 }
+
