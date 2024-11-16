d6a910e1034c85cb447483029e23e5f1fbfc7ec3
Gabriel Garcia <gabgac2000@outlook.com>
2024-11-16 17:56:54 -0500

    labs 17

---
+++ Laboratorio17/Laboratorio171/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Laboratorio171._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+        <div class="row">
+            <asp:GridView ID="MyGridView" DataSourceID="MyDataSource1" 
+                AllowSorting="true" AllowPaging="true"
+                DataKeyNames="ProductId" AutoGenerateEditButton="false" Runat="server"/>
+            <asp:SqlDataSource ID="MyDataSource1" runat="server"
+                ConnectionString="Data Source=localhost; Initial Catalog=Northwind; Integrated Security=SSPI;"
+                ProviderName="System.Data.SqlClient"
+                SelectCommand="SELECT ProductId, ProductName,UnitPrice from Products"
+                UpdateCommand="UPDATE Products SET ProductName=@ProductName, UnitPrice=@UnitePrice WHERE ProductId=@ProductId">
+                </asp:SqlDataSource>
+        </div>
+
+</asp:Content>
---
+++ Laboratorio17/Laboratorio171/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboratorio171
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                // Obtiene la cadena de conexión desde el Web.config
+                ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["ConexionNorthwind"];
+                using (SqlConnection conexion = new SqlConnection(connString.ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";
+
+                        conexion.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                DataTable dt = new DataTable();
+                                dt.Load(reader);
+                                GridV.DataSource = dt;
+                                GridV.DataBind();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                Response.Write($"Error: {ex.Message}");
+            }
+        }
+    }
+}
