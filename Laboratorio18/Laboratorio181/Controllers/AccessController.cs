e0eb1b5f0c306e47d77d7df8db16674edc73db5a
Gabriel Garcia <gabgac2000@outlook.com>
2024-11-18 11:06:26 -0500

    Laboratorio 18 listo

---
+++ Laboratorio18/Laboratorio181/Controllers/AccessController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio181.Controllers
+{
+    public class AccessController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string pass)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrrio un error:("+ ex.Message);
+            }
+        }
+    }
+}
